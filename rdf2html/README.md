ae0ef449897080c2bd3a76a43eec07bbdf2daf68
Luca Roffia <lroffia@gmail.com>
2026-01-27 14:15:28 +0100

    fix working on JSAP ontology

---
+++ rdf2html/README.md
@@ -0,0 +1,9 @@
+# My2Sec ontologies
+
+HTML files are automatically rendered from RDF sources. To render them:
+
+1. ``install Node.js`` (if necessary)
+2. ``npm i`` (to do once to install dependencies)
+3. ``node render.js``
+
+Rendering is performed according to ``vaimee.rq``. Please refer [here](https://ns.inria.fr/sparql-template/) for a complete documentation.
---
+++ rdf2html/render.js
@@ -0,0 +1,36 @@
+const ontologies = [
+    {'../jsap/jsap_ontology.ttl':"http://vaimee.com/template#jsap"}];
+    
+let fs = require('fs');
+let sttl = require('sttl');
+let urdf = require('urdf');
+
+const tpl = fs.readFileSync('vaimee.rq', 'utf-8');
+
+sttl.register(tpl);
+sttl.connect(q => {
+    return urdf.query(q)
+    .then(b => ({
+        results: {
+            bindings: b
+        }
+    }));
+});
+
+console.log("Rendering ontology documentation...");
+const promiseChain = ontologies.reduce((p, src) => {
+    return p.then( _ => render(Object.keys(src)[0],Object.values(src)[0],Object.keys(src)[0].replace('.ttl', '.html'))).then(() => urdf.clear())
+}, Promise.resolve());
+
+
+function render(ontologyFile,templateURI,output) {
+    let ttl = fs.readFileSync(ontologyFile, 'UTF-8');
+    return Promise.resolve()
+    .then(() => urdf.load(ttl, { format: 'text/turtle' }))
+    .then(() => sttl.callTemplate(templateURI, ttl))
+    .then(html => fs.writeFileSync(output, html))
+    .then(_ => console.log("File ",output,"generated"))
+    .catch((e) => {
+        console.error('Error while rendering ' + ontologyFile + ': ' + e);
+    });
+}
